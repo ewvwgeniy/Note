5c46084be36b2ca59cddc8d8375687b0367ee557
ewvwgeniy <65627320+ewvwgeniy@users.noreply.github.com>
2022-10-15 01:50:23 +0300

    Updates

---
+++ precache-manifest.d006eeda120f7af90be530aa8e4cb49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b356583a5ec9af7ef7e",
+    "url": "/static/css/main.aeeefcf8.chunk.css"
+  },
+  {
+    "revision": "9b356583a5ec9af7ef7e",
+    "url": "/static/js/main.9b356583.chunk.js"
+  },
+  {
+    "revision": "05d202352e6b34541f90",
+    "url": "/static/js/1.05d20235.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2893d37b570a397a042eca40464a74e7",
+    "url": "/index.html"
+  }
+];
