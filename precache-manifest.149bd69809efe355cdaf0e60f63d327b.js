3c30fdad41cf479a7c1a26563a68f04a8ad5e7f1
ewvwgeniy <65627320+ewvwgeniy@users.noreply.github.com>
2022-10-15 01:53:52 +0300

    Updates

---
+++ precache-manifest.149bd69809efe355cdaf0e60f63d327b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b356583a5ec9af7ef7e",
+    "url": "/Note/static/css/main.aeeefcf8.chunk.css"
+  },
+  {
+    "revision": "9b356583a5ec9af7ef7e",
+    "url": "/Note/static/js/main.9b356583.chunk.js"
+  },
+  {
+    "revision": "05d202352e6b34541f90",
+    "url": "/Note/static/js/1.05d20235.chunk.js"
+  },
+  {
+    "revision": "476e93b32326164b24d0",
+    "url": "/Note/static/js/runtime~main.476e93b3.js"
+  },
+  {
+    "revision": "edcba149651c51fda584dbc290ac1f63",
+    "url": "/Note/index.html"
+  }
+];
